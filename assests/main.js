7430350f554059cff550e1a0ce2ec72e918e1a6f
Nisa Najafli <ncfliniss@gmail.com>
2025-12-09 12:13:14 +0400

    update

--- assests/main.js
+++ assests/main.js
@@ -24,3 +24,25 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+let swiperProjects = new Swiper(".home-projects22", {
+      spaceBetween: 20,
+      slidesPerView: 3,
+      // autoplay: {
+      //   delay: 2500,
+      //   disableOnInteraction: false,
+      // },
+      breakpoints: {
+      320: { slidesPerView: 1.3 },
+      576: { slidesPerView: 2 },
+      768: { slidesPerView: 3 },
+    },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".home-projects22 .swiper-button-next",
+        prevEl: ".home-projects22 .swiper-button-prev",
+      },
+    });
+    document.getElementById("year").textContent = new Date().getFullYear()
